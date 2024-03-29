3886f6b384aa025b54f1746d728572483571cfb7
Gina Sohn <gina7484@gmail.com>
2024-03-29 15:31:58 +0000

    Add comparison script for lang diff too

--- speedup_breakdown.sh
+++ speedup_breakdown.sh
@@ -13,18 +13,23 @@ run_single() {
     VAR1="$(egrep "finished in" rDAM_run_$LEN.txt)"
     value1="${VAR1#*'d in'}" # take everything after the 'd in'
     echo "[Restricted DAM] Real Time: $value1"
-    value_a="${value1%'s'*}" # take everything after the 's'
+    value_a="${value1%'s'*}" # take everything before the 's'
     rm rDAM_run_$LEN.txt
 
     cd /home/dam/dam-experiments/stream-attn-dam/
     VAR2="$(egrep "finished in" DAM_run_$LEN.txt)"
     value2="${VAR2#*'d in'}" # take everything after the 'd in'
-    value_b="${value2%'s'*}" # take everything after the 's'
+    value_b="${value2%'s'*}" # take everything before the 's'
     echo "           [DAM] Real Time: $value2"
     rm DAM_run_$LEN.txt
 
     cd /home/dam/dam-experiments/stream-attn-r-dam/
-    python speedup_calc.py $value_a $value_b
+    python speedup_calc.py $value_a $value_b par
+
+    SPATIAL="$(grep $LEN spatial_sweep.txt)"
+    SPATIAL1="${SPATIAL#*'= '}" # take everything after the '='
+    SPATIAL2="${SPATIAL1%'s'*}" # take everything before the 's'
+    python speedup_calc.py $SPATIAL2 $value_a lang
     echo ""
     echo ""
 }
--- speedup_calc.py
+++ speedup_calc.py
@@ -3,4 +3,7 @@ num1 = float(sys.argv[1])
 num2 = float(sys.argv[2])
  
 # printing the sum in integer
-print("Speed up from parallelism in the framework: {}x".format(round(num1 / num2, 2)))
+if sys.argv[3] == 'par':
+    print("Speed up from parallelism in the framework: {}x".format(round(num1 / num2, 2)))
+if sys.argv[3] == 'lang':
+    print("Speed up from the language difference     : {}x".format(round(num1 / num2, 2)))
