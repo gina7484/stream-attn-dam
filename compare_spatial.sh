37af6e21a8a961530f309abf3f5f098a7cac1445
Gina Sohn <gina7484@gmail.com>
2024-03-29 13:30:22 +0000

    create script to do sweep for DAM (fig 5,6)

--- compare_spatial.sh
+++ compare_spatial.sh
@@ -1,1 +1,38 @@
-cargo test --release --package stream-attn-dam --lib -- 512 test::naive_attn::tests::qkt_red_div_matvec_test --exact --nocapture
+#!/bin/bash
+
+run_single() {
+    echo "Running naive-attention for $LEN"
+    cargo test --release --package stream-attn-dam --lib -- $LEN test::naive_attn::tests::qkt_red_div_matvec_test --exact --nocapture > DAM_run_$LEN.txt
+    
+    VAR="$(egrep "finished in" DAM_run_$LEN.txt)"
+    value1="${VAR#*'d in'}" # take everything after the 'd in'
+    # valueA2="${valueA1%(*}" # take everything before the '('
+    echo "Real Time: $value1"
+
+    grep Simulated DAM_run_$LEN.txt
+    echo ""
+}
+
+cd /home/dam/dam-experiments/stream-attn-dam/
+
+# The total sweep will be 3.5 -4 hr
+# LEN=512     # 0.3 sec
+# run_single
+
+# LEN=1024    # 1.4 sec
+# run_single
+
+# LEN=2048    # 5 sec
+# run_single
+
+# LEN=4096    # 21 sec
+# run_single
+
+# LEN=8192    # 84 sec
+# run_single
+
+# LEN=16384   # 5.5 min
+# run_single
+
+LEN=32768   #  min
+run_single
